15a0a30cb61a6d3e10e3da8e1324586f6c733e8a
Lucas N Ferreira <lucasnf99@gmail.com>
2026-01-28 21:12:42 -0300

    fix: create sections folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,16 @@
-import About from "../components/About";
-import { CTA } from "../components/Cta";
-import { FAQ } from "../components/Faq";
-import { Highlights } from "../components/Highlights";
-import { Services } from "../components/Services";
-import Hero from "../components/Hero";
 import Header from "../components/Header";
+import Hero from "../components/Sections/Hero";
+import About from "../components/Sections/About";
+import { Services } from "../components/Sections/Services";
+import { Highlights } from "../components/Sections/Highlights";
+import { FAQ } from "../components/Sections/Faq";
+import { CTA } from "../components/Sections/Cta";
 
 export default function Home() {
     return (
         <>
             <Header />
             <main className="pt-16">
-
                 <Hero />
                 <About />
                 <Services />
